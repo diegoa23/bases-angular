1f9efc64ad98a6895d1d60ff80ea2355dcb088d2
Diego Ayala <namadian13@gmail.com>
2024-04-09 23:13:01 -0400

    diego

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DBZModule } from './dbz/dbz.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    DBZModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
